dcd2879e656394ed7ebdc84b2de31d7deb8c4761
XDargu <xdargu@gmail.com>
2025-05-01 02:21:29 +0200

    Added global presets and variations

--- src/generation.js
+++ src/generation.js
@@ -55,15 +55,181 @@ const presets = [
             { symbol: 'F', odds: 1.0, newSymbols: 'FF' },
         ]
     },
+    {
+        axiom: 'FFX',
+        rules: [
+            { symbol: 'X', odds: 1.0, newSymbols: 'F+[-F-XF-X][+FF][--XF[+X]][++F-X]' },
+            { symbol: 'F', odds: 1.0, newSymbols: 'FF' },
+        ]
+    },
+    {
+        axiom: 'FX',
+        rules: [
+            { symbol: 'X', odds: 0.5, newSymbols: 'FFF[]+[]+[]+[]+[]+[]+[]-------[]-[]-[]-[]-[]-[]' },
+            { symbol: 'X', odds: 0.5, newSymbols: 'F+[-F-XF-X][+FF][--XF[+X]][++F-X]' },
+            { symbol: 'F', odds: 1.0, newSymbols: 'FF' },
+        ]
+    }
 ];
 
+const globalPresets = [
+    {
+        backgroundColorTop: "#ddfafd",
+        backgroundColorBottom: "#fecba9",
+        preset: 1,
+        iterations: 3,
+        variability: 0.1,
+        seed: 25,
+        isAnimated: false,
+        leafColor: "#5edb3b",
+        leafType: 0,
+        leafLength: 10,
+        leafWidth: 5,
+        leafAlpha: 0.75,
+        branchColor: "#7d5010",
+        branchLength: 30,
+        branchAngle: 22.5,
+        branchWidth: 6,
+        branchWidthFalloff: 0.7, 
+        branchWidthMin: 0.2
+    },
+    {
+        backgroundColorTop: "#1b44a1",
+        backgroundColorBottom: "#ff7452",
+        preset: 1,
+        iterations: 3,
+        variability: 0.38,
+        seed: 17,
+        isAnimated: false,
+        leafColor: "#f9f110",
+        leafType: 1,
+        leafLength: 13.5,
+        leafWidth: 11.5,
+        leafAlpha: 0.73,
+        branchColor: "#7d5010",
+        branchLength: 26.5,
+        branchAngle: 19.6,
+        branchWidth: 13.3,
+        branchWidthFalloff: 0.74, 
+        branchWidthMin: 3.3
+    },
+    {
+        backgroundColorTop: "#c4e8ee",
+        backgroundColorBottom: "#ffffff",
+        preset: 1,
+        iterations: 4,
+        variability: 0.55,
+        seed: 27,
+        isAnimated: false,
+        leafColor: "#ff8fe7",
+        leafType: 0,
+        leafLength: 8.5,
+        leafWidth: 12.5,
+        leafAlpha: 0.76,
+        branchColor: "#504534",
+        branchLength: 15.5,
+        branchAngle: 27.1,
+        branchWidth: 38.8,
+        branchWidthFalloff: 0.56, 
+        branchWidthMin: 2.7
+    },
+    {
+        backgroundColorTop: "#ebebeb",
+        backgroundColorBottom: "#c7dfff",
+        preset: 4,
+        iterations: 2,
+        variability: 0.16,
+        seed: 39,
+        isAnimated: false,
+        leafColor: "#b76f0b",
+        leafType: 2,
+        leafLength: 16.5,
+        leafWidth: 10.5,
+        leafAlpha: 1,
+        branchColor: "#3dc60c",
+        branchLength: 68.5,
+        branchAngle: 27.8,
+        branchWidth: 8.4,
+        branchWidthFalloff: 0.53, 
+        branchWidthMin: 0.1
+    },
+    {
+        backgroundColorTop: "#eedddf",
+        backgroundColorBottom: "#81c589",
+        preset: 7,
+        iterations: 3,
+        variability: 0.1,
+        seed: 25,
+        isAnimated: false,
+        leafColor: "#215a11",
+        leafType: 1,
+        leafLength: 16.5,
+        leafWidth: 3.5,
+        leafAlpha: 0.63,
+        branchColor: "#7d5010",
+        branchLength: 18.5,
+        branchAngle: 29.9,
+        branchWidth: 24.1,
+        branchWidthFalloff: 0.46, 
+        branchWidthMin: 0.1
+    },
+    {
+        backgroundColorTop: "#72bedf",
+        backgroundColorBottom: "#ffdeb8",
+        preset: 6,
+        iterations: 4,
+        variability: 0.4,
+        seed: 39,
+        isAnimated: false,
+        leafColor: "#227b0a",
+        leafType: 2,
+        leafLength: 10.5,
+        leafWidth: 0.5,
+        leafAlpha: 0.57,
+        branchColor: "#7d5010",
+        branchLength: 12.5,
+        branchAngle: 19.6,
+        branchWidth: 20,
+        branchWidthFalloff: 0.64, 
+        branchWidthMin: 0.1
+    }
+]
+
 let lastTimeout = null;
+
+function LoadGlobalPreset()
+{
+    const preset = globalPresets[document.getElementById("globalPresets").valueAsNumber];
+
+    // Params
+    document.getElementById("background-col-top").value = preset.backgroundColorTop;
+    document.getElementById("background-col-bottom").value = preset.backgroundColorBottom;
+    document.getElementById("presets").value = preset.preset;
+    document.getElementById("iterations").value = preset.iterations;
+    document.getElementById("variability").value = preset.variability;
+    document.getElementById("seed").value = preset.seed;
+    document.getElementById("animate").checked = preset.isAnimated;
+
+    document.getElementById("leaves-col").value = preset.leafColor;
+    document.getElementById("leaves-type").value = preset.leafType;
+    document.getElementById("leaves-length").value = preset.leafLength;
+    document.getElementById("leaves-width").value = preset.leafWidth;
+    document.getElementById("leaves-alpha").value = preset.leafAlpha;
+
+    document.getElementById("branches-col").value = preset.branchColor;
+    document.getElementById("branches-length").value = preset.branchLength;
+    document.getElementById("branches-angle").value = preset.branchAngle;
+    document.getElementById("branches-width").value = preset.branchWidth;
+    document.getElementById("branches-width-falloff").value = preset.branchWidthFalloff;
+    document.getElementById("branches-min-width").value = preset.branchWidthMin;
+}
 
 function Run(useUserSentence = false)
 {
     let config = {};
     // Params
-    config.backgroundColor = document.getElementById("background-col").value;
+    config.backgroundColorTop = document.getElementById("background-col-top").value;
+    config.backgroundColorBottom = document.getElementById("background-col-bottom").value;
     config.preset = document.getElementById("presets").valueAsNumber;
     config.iterations = document.getElementById("iterations").valueAsNumber;
     config.variability = document.getElementById("variability").valueAsNumber;
@@ -80,8 +246,8 @@ function Run(useUserSentence = false)
     config.branchLength = document.getElementById("branches-length").valueAsNumber;
     config.branchAngle = document.getElementById("branches-angle").valueAsNumber;
     config.branchWidth = document.getElementById("branches-width").valueAsNumber;
-    config.branchWidthMin = document.getElementById("branches-min-width").valueAsNumber;
     config.branchWidthFalloff = document.getElementById("branches-width-falloff").valueAsNumber;
+    config.branchWidthMin = document.getElementById("branches-min-width").valueAsNumber;
 
     config.rng = new RNG(config.seed);
 
@@ -248,8 +414,15 @@ function* Render(config, updateSentence = false)
 
     ctx.resetTransform();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = config.backgroundColor;
+    
+    // Background
+    const grad = ctx.createLinearGradient(0, 0, 0, canvas.height * 0.7);
+    grad.addColorStop(0, config.backgroundColorTop);
+    grad.addColorStop(1, config.backgroundColorBottom);
+
+    ctx.fillStyle = grad;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.transform(1, 0, 0, 1, canvas.width / 2, canvas.height);
 
     const textArea = document.getElementById("sentence");
@@ -314,6 +487,12 @@ function OnParamsChanged()
 function OnSentenceChanged()
 {
     Run(true);
+}
+
+function OnGlobalPresetChanged()
+{
+    LoadGlobalPreset();
+    Run();
 }
 
 Run();
